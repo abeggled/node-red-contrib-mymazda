dd408ca04d0f60ba4f0fbbc5a3cb340552e126a4
abeggled <abeggled@users.noreply.github.com>
2023-03-14 13:14:57 +0100

    Initial

---
+++ red/mymazda-account.js
@@ -0,0 +1,59 @@
+module.exports = function (RED) {
+  let Accounts = new Map()
+  let MazdaClient = require('node-mymazda')
+
+  function MyMazdaAccount(config) {
+    RED.nodes.createNode(this, config)
+
+    // Setup client/connection
+    try {
+      this.client = new MazdaClient(config.email, this.credentials.password, config.region)
+    } catch (err) {
+      this.error(err.message, err.stack)
+      return
+    }
+
+    Accounts.set(config.email, this)
+
+    this.listVehicles = async () => {
+      try {
+        // Gets vehicles and stores in cache
+        this.vehicles = await this.client.getVehicles()
+      } catch (err) {
+        this.error(err)
+      }
+    }
+
+    // Get list of vehicles
+    this.listVehicles()
+  }
+
+  RED.nodes.registerType('mymazda-account', MyMazdaAccount, {
+    credentials: {
+      password: { type: 'password' }
+    }
+  })
+
+  RED.httpAdmin.get('/mazda-client/vehicles', async function (req, res) {
+    let { email, refetch } = req.query
+
+    if (!email) {
+      res.status(500).send('Missing arguments')
+      return
+    }
+
+    if (!Accounts.has(email)) {
+      res.status(500).send('Account not ready')
+      return
+    }
+
+    let account = Accounts.get(email)
+
+    // If refetch is true, vehicle list is fetched again; otherwise cache is used
+    if (refetch) await account.listVehicles()
+
+    res.set({ 'content-type': 'application/json; charset=utf-8' })
+    res.end(JSON.stringify(account.vehicles))
+    return
+  })
+}
